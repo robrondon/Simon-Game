8644c11469040b173298f5e8119d5a8cb580dd22
Robert Rondon <robrondon11@gmail.com>
2021-12-28 14:15:35 -0300

    Finished the game logic

--- game.js
+++ game.js
@@ -4,7 +4,89 @@ let gamePattern = [];
 
 let userClickedPattern = [];
 
+let started = false;
+
+let level = 0;
+
+$( document ).on( 'keydown', function () {
+
+  if ( !started ) {
+
+    $( '#level-title' ).text( `Level ${level}` );
+    nextSequence();
+    started = true;
+
+  } 
+})
+
+$( '.btn' ).on( 'click', function () {
+  
+  let userChosenColour = $(this).attr('id');
+
+  userClickedPattern.push( userChosenColour );
+
+  playSound( userChosenColour );
+
+  animatePress( userChosenColour );
+
+  checkAnswer( userClickedPattern.length - 1 );
+
+} )
+
+const checkAnswer = function ( currentLevel ) {
+
+  if ( userClickedPattern[currentLevel] === gamePattern[currentLevel] ) {
+
+    console.log( 'success' );
+    console.log( userClickedPattern );
+    console.log( gamePattern );
+    console.log( userClickedPattern.length );
+    console.log( gamePattern.length );
+
+    if ( userClickedPattern.length === gamePattern.length ) {
+
+      setTimeout( function () {
+        nextSequence();
+      }, 1000 )
+      
+    }
+
+  } else {
+
+    console.log( 'wrong' );
+    console.log( userClickedPattern, gamePattern );
+
+    playSound( 'wrong' );
+
+    $( 'body' ).addClass( 'game-over' );
+
+    setTimeout( function () {
+
+      $( 'body' ).removeClass( 'game-over' )
+
+    }, 200 );
+    
+    $( '#level-title' ).text( 'Game Over, Press Any Key to Restart' );
+
+    startOver();
+  }
+}
+
+const startOver = function () {
+  level = 0;
+  gamePattern = [];
+  started = false;
+
+  console.log( level, gamePattern, started );
+}
+
 const nextSequence = function () {
+
+  userClickedPattern = [];
+
+  level++;
+
+  $( '#level-title' ).text( `Level ${level}` );
   
   let randomNumber = Math.floor( Math.random() * 4 );
   
@@ -20,16 +102,19 @@ const nextSequence = function () {
 const playSound = function ( name ) {
   
   let audio = new Audio( `./sounds/${name}.mp3` );
-  
+
   audio.play();
 
 } 
 
-$( '.btn' ).on( 'click', function () {
-  
-  let userChosenColour = this.id;
+const animatePress = function ( currentColour ) {
 
-  userClickedPattern.push( userChosenColour );
+  $( `#${currentColour}` ).addClass( 'pressed' );
 
-  playSound( userChosenColour );
-})
+  setTimeout( function () {
+
+    $( `#${currentColour}` ).removeClass( 'pressed' );
+
+  }, 100 );
+}
+
